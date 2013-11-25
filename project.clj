0f5f1c069e6a0d88d68f10e747e17d314f9115c5
kevinbuch <kevaustinbuch@gmail.com>
2013-11-25 14:04:15 -0600

    updated ttt jar

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject tic-tac-toe-console "0.1.1"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.4"]
-                 [tic-tac-toe "0.1.0-SNAPSHOT"]]
+                 [org.clojars.kbuchanan/tic-tac-toe "0.1.1"]]
   :main tic-tac-toe-console.core
   :profiles {:dev {:dependencies [[speclj "2.5.0"]]}}
   :plugins [[speclj "2.5.0"]]
